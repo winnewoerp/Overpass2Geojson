e6c9ffeb981b511fa7b5fe2fe9a76cf22f036642
STADTKREATION <git@stadtkreation.de>
2021-09-15 02:49:13 +0200

    Added relation conversion, first part

--- lib/Overpass2Geojson.php
+++ lib/Overpass2Geojson.php
@@ -35,6 +35,43 @@ class Overpass2Geojson {
         }
         return $encode ? json_encode($output) : $output;
     }
+	
+    /**
+     * Converts a JSON string or decoded array into a GeoJSON string or array.
+     * This creates a Polygon feature for each supplied relation with member ways
+	 * and their respective nodes as points
+     * @param  mixed   $input  JSON string or array
+     * @param  boolean $encode whether to encode output as string
+     * @return mixed           false if failed, otherwise GeoJSON string or array
+     */
+    public static function convertRelations($input, $encode = true, $polygon = false) {
+	    
+	    self::$polygon = $polygon;
+	    
+        $inputArray = self::validateInput($input);
+        if (!$inputArray) {
+            return false;
+        }
+		$output = array(
+            'type' => 'FeatureCollection',
+            'features' => array(),
+        );
+		$nodes = self::collectNodes($inputArray['elements']);
+
+        foreach ($inputArray['elements'] as $osmItem) {
+            if (isset($osmItem['type']) && $osmItem['type'] === 'relation') {
+				$ways = array();
+				foreach ($osmItem['members'] as $member) {
+					$feature = self::createMemberFeature($inputArray['elements'][array_search($member['ref'], array_column($inputArray['elements'], 'id'))], $nodes);
+					if ($feature) {
+						$ways[] = $feature;
+					}
+				}
+				$output['features'][] = self::createRelationFeature($osmItem, $ways);
+            }
+        }
+        return $encode ? json_encode($output) : $output;
+    }
 
     /**
      * Converts a JSON string or decoded array into a GeoJSON string or array.
@@ -126,6 +163,46 @@ class Overpass2Geojson {
                 'properties' => isset($way['tags']) ? array_merge($way['tags'], ["id"=>$way['id']]) : ["id"=>$way['id']],
             );
         }
+        return false;
+    }
+	
+    /**
+     * Creates a Feature array with geometry from matching ways
+     * @param  array $relation OSM relation
+     * @param  array $ways	   OSM ways as members of relation
+     * @return mixed           false if invalid feature otherwise
+     *                         array GeoJSON Feature with Polygon geometry
+     */
+    public static function createRelationFeature($relation, $ways) {
+		return array(
+			'type' => 'Feature',
+			'geometry' => array(
+				'type' => 'Polygon',
+				'coordinates' => $ways,
+			),
+			'properties' => isset($relation['tags']) ? array_merge($relation['tags'], ["id"=>$relation['id']]) : ["id"=>$relation['id']],
+		);
+	}
+	
+	/**
+     * Creates a Relation member Feature array with geometry from matching nodes
+     * @param  array $way  OSM way
+     * @param  array $nodes    OSM node coordinates indexed by id
+     * @return mixed           false if invalid feature otherwise
+     *                         array with coordinate pairs of relation member
+     */
+    public static function createMemberFeature($way, $nodes) {
+        $coords = array();
+        if (isset($way['nodes'])) {
+            foreach ($way['nodes'] as $nodeId) {
+                if (isset($nodes[$nodeId])) {
+                    $coords[] = array($nodes[$nodeId]['lon'], $nodes[$nodeId]['lat']);
+                }
+            }
+        }
+        if (count($coords) >= 2) {
+            return $coords;
+		}
         return false;
     }
 }
